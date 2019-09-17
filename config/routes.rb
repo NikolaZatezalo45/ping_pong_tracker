286af88d70c4159da84e3d579c2c195f9dd0fca5
Nikola Zatezalo <tesla.zatezalo@gmail.com>
2019-09-17 14:20:11 +0200

    devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :players
-  root to: 'players#index'
+
+  devise_scope :user do
+    authenticated :user do
+     root 'players#index', as: :authenticated_root
+    end
+    unauthenticated do
+     root 'devise/sessions#new', as: :unauthenticated_root
+   end
+  end
 end
