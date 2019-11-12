569ba50718fc0bcc042cdf8d8cbe1e221e1b3bcd
Nikola Zatezalo <tesla.zatezalo@gmail.com>
2019-11-12 15:46:37 +0200

    added delete match button

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,5 @@
 class MatchesController < ApplicationController
-  before_action :set_match, only: [:show]
+  before_action :set_match, only: [:show, :destroy]
 
   def index
     @matches = Match.all
@@ -28,6 +28,36 @@ class MatchesController < ApplicationController
 
   def show
   end
+  def destroy
+    @player = Player.find(@match.winner_id)
+    @player.score = @player.score-1   
+    @player.save
+    redirect_to players_path if @match.destroy
+  end
+  # def edit
+
+  # end
+
+  # def update
+  #   respond_to do |format|
+  #     match_params[:player_1_score] > match_params[:player_2_score] ? @winner = match_params[:player_1_id] : @winner = match_params[:player_2_id] 
+  #     if @match.winner_id != @winner
+  #       @player = Player.find(@match.winner_id)
+  #       @player.score = @player.score-1   
+  #       @player.save
+  #       @player = Player.find(@winner)
+  #       @player.score = @player.score+1   
+  #       @player.save
+  #     end
+  #     if @match.update(match_params.merge(winner: Player.find(@winner)))
+  #       format.html { redirect_to @match, notice: 'Match was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @match }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @match.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   private
 
